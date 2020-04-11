01cc57735471bdb044236c0dd6e4ac52813e5a73
James Leu <leujase@gmail.com>
2020-04-11 13:09:48 -0500

    completed coding fruit store assignment

--- server.py
+++ server.py
@@ -8,7 +8,16 @@ def index():
 @app.route('/checkout', methods=['POST'])         
 def checkout():
     print(request.form)
-    return render_template("checkout.html")
+    strawberry = request.form['strawberry']
+    raspberry = request.form['raspberry']
+    apple = request.form['apple']
+    blackberry = request.form['blackberry']
+    firstName = request.form['first_name']
+    lastName = request.form['last_name']
+    studentId = request.form['student_id']
+    total = int(apple) + int(blackberry) + int(strawberry) + int(raspberry)
+    print(f'Charging {firstName} {lastName} for {total} fruits')
+    return render_template("checkout.html", strawberry = strawberry, raspberry = raspberry, blackberry = blackberry, apple = apple, firstName = firstName, lastName = lastName, studentId = studentId)
 
 @app.route('/fruits')         
 def fruits():
